478982b505edc1fcc1119ef8cd952a391e62ee9c
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-06 11:33:10 -0500

    Added more tasks to test in Run004.

--- Proj_OrganizerApp_Vers3/run/org/cdahmedeh/orgapp/runs/Run004_TasksSetToTimeBlocks.java
+++ Proj_OrganizerApp_Vers3/run/org/cdahmedeh/orgapp/runs/Run004_TasksSetToTimeBlocks.java
@@ -12,11 +12,23 @@ import org.joda.time.DateTime;
 public class Run004_TasksSetToTimeBlocks {
 	public static void main(String[] args) throws Exception {
 		Task task = new Task();
-		task.setTitle("Some Title");
+		task.setTitle("Two blocks");
 		
 		task.assignToTimeBlock(new TimeBlock());
 		task.assignToTimeBlock(new TimeBlock(new DateTime().plusDays(3)));
 		
 		System.out.println(task.toString());
+		
+		Task task2 = new Task();
+		task2.setTitle("No Time");
+		
+		System.out.println(task2.toString());
+		
+		Task task3 = new Task();
+		task3.setTitle("One block");
+		
+		task3.assignToTimeBlock(new TimeBlock(new DateTime().plusDays(1), new DateTime().plusDays(1).plusHours(1)));
+		
+		System.out.println(task3.toString());
 	}
 }
