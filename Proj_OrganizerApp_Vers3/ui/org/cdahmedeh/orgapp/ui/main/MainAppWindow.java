3891090ab21c568456985e0b4b46360270d545ec
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-08 15:02:27 -0500

    Init UI. Added swt dependencies and MainAppWindow empty window.

---
+++ Proj_OrganizerApp_Vers3/ui/org/cdahmedeh/orgapp/ui/main/MainAppWindow.java
@@ -0,0 +1,36 @@
+package org.cdahmedeh.orgapp.ui.main;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class MainAppWindow {
+	protected Shell shell;
+
+	public static void main(String[] args) {
+		try {
+			MainAppWindow window = new MainAppWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+
+	}
+}
