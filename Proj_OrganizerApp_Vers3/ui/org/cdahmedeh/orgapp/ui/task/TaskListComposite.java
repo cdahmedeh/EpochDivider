155772ff47569bc7a144a1844a325b83fb6f62d4
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-08 15:07:24 -0500

    Init. TaskListComposite for Task List component.

---
+++ Proj_OrganizerApp_Vers3/ui/org/cdahmedeh/orgapp/ui/task/TaskListComposite.java
@@ -0,0 +1,11 @@
+package org.cdahmedeh.orgapp.ui.task;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class TaskListComposite extends Composite {
+	@Override protected void checkSubclass() {}
+	
+	public TaskListComposite(Composite parent, int style) {
+		super(parent, style);
+	}
+}
