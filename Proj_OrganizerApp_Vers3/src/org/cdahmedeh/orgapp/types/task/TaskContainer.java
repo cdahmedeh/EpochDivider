7f89515993948553335bef1caf27b7715fdb2358
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-08 23:58:16 -0500

    Added TaskContainer for holding Task instances.

---
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/task/TaskContainer.java
@@ -0,0 +1,10 @@
+package org.cdahmedeh.orgapp.types.task;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TaskContainer {
+	public ArrayList<Task> tasks = new ArrayList<>();
+	public void addTask(Task task) {tasks.add(task);}
+	public ArrayList<Task> getAllTasks() {return tasks;} //TODO: Should we clone?
+}
