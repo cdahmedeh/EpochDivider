ac28c8c17dbc7f668ec3bb6ee890ed56600e3d75
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-11 12:46:46 -0400

    Code Cleanup Pass 1

--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/Category.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/Category.java
@@ -1,15 +1,12 @@
 package org.cdahmedeh.orgapp.types.category;
 
 public class Category {
+	public Category(String name) {this.setName(name); id = idCounter++;}
+
 	private static int idCounter = 0;
 	private int id = -1;
-	public int getId() {
-		return id;
-	}
-	
-	public Category() {}
-	public Category(String name) {this.setName(name); id = idCounter++;}
-	
+	public int getId() {return id;}
+
 	private String name = "";
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name != null ? name.trim() : "";}
