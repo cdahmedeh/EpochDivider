459f8e41b0eeb3246e160159bb7cdba0cf67fa9f
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-11 12:51:33 -0400

    Code Cleanup Pass 1

--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/task/TaskContainer.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/task/TaskContainer.java
@@ -5,14 +5,10 @@ import java.util.ArrayList;
 public class TaskContainer {
 	public ArrayList<Task> tasks = new ArrayList<>();
 	public void addTask(Task task) {tasks.add(task);}
-	public ArrayList<Task> getAllTasks() {return tasks;} //TODO: Should we clone?
+	public ArrayList<Task> getAllTasks() {return tasks;}
 	
 	public Task getTaskFromId(int id){
-		for (Task task: tasks) {
-			if (task.getId() == id){
-				return task;
-			}
-		}
+		for (Task task: tasks) if (task.getId() == id) return task;
 		return null;
 	}
 }
