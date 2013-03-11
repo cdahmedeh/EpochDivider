faa2c88a8ae555cf28b0c224e9cbdafe6b197dc1
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-11 12:41:29 -0400

    Basic Id system for Categories.

--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/Category.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/Category.java
@@ -1,8 +1,14 @@
 package org.cdahmedeh.orgapp.types.category;
 
 public class Category {
+	private static int idCounter = 0;
+	private int id = -1;
+	public int getId() {
+		return id;
+	}
+	
 	public Category() {}
-	public Category(String name) {this.setName(name);}
+	public Category(String name) {this.setName(name); id = idCounter++;}
 	
 	private String name = "";
 	public String getName() {return name;}
--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/CategoryContainer.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/category/CategoryContainer.java
@@ -3,9 +3,19 @@ package org.cdahmedeh.orgapp.types.category;
 import java.util.ArrayList;
 
 import org.cdahmedeh.orgapp.types.category.Category;
+import org.cdahmedeh.orgapp.types.task.Task;
 
 public class CategoryContainer {
 	public ArrayList<Category> categories = new ArrayList<>();
 	public void addCategory(Category category) {categories.add(category);}
 	public ArrayList<Category> getAllCategorys() {return categories;} //TODO: Should we clone?
+	
+	public Category getCategoryFromId(int id){
+		for (Category category: categories) {
+			if (category.getId() == id){
+				return category;
+			}
+		}
+		return null;
+	}
 }
