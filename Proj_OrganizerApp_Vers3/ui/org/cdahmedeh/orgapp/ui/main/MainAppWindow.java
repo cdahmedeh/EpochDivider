3eec5481a91b62db8b22362e3bbf3a3745a22aff
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-11 10:29:55 -0400

    Make use of new Category List in Main Window.

--- Proj_OrganizerApp_Vers3/ui/org/cdahmedeh/orgapp/ui/main/MainAppWindow.java
+++ Proj_OrganizerApp_Vers3/ui/org/cdahmedeh/orgapp/ui/main/MainAppWindow.java
@@ -1,9 +1,12 @@
 package org.cdahmedeh.orgapp.ui.main;
 
+import org.cdahmedeh.orgapp.types.category.Category;
+import org.cdahmedeh.orgapp.types.category.CategoryContainer;
 import org.cdahmedeh.orgapp.types.task.Task;
 import org.cdahmedeh.orgapp.types.task.TaskContainer;
 import org.cdahmedeh.orgapp.types.time.TimeBlock;
 import org.cdahmedeh.orgapp.ui.calendar.CalendarComposite;
+import org.cdahmedeh.orgapp.ui.category.CategoryListComposite;
 import org.cdahmedeh.orgapp.ui.task.TaskListComposite;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
@@ -12,6 +15,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
@@ -42,20 +46,30 @@ public class MainAppWindow {
 	protected void createContents() {
 		shell = new Shell();
 		shell.setSize(800, 600);
-		shell.setText("SWT Application");
+		shell.setText("Epoch Divider");
 
 		shell.setLayout(new FillLayout());
 		
+		CategoryContainer categoryContainer = new CategoryContainer();
+		categoryContainer.addCategory(new Category("Essentials"));
+		categoryContainer.addCategory(new Category("University"));
+		
 		TaskContainer taskContainer = new TaskContainer();
 		Task task = new Task("Test");
-		task.assignToTimeBlock(new TimeBlock());
-		task.assignToTimeBlock(new TimeBlock(new LocalDate().toDateTime(new LocalTime(14,30,00))));
+//		task.assignToTimeBlock(new TimeBlock());
+		task.assignToTimeBlock(new TimeBlock(new DateTime(2013, 03, 12, 12, 00), new DateTime(2013, 03, 13, 12, 00)));
 		taskContainer.addTask(task);
 		taskContainer.addTask(new Task("Test"));
 		
-		SashForm sashForm = new SashForm(shell, SWT.SMOOTH | SWT.VERTICAL);
+		SashForm fullSashForm = new SashForm(shell, SWT.SMOOTH | SWT.HORIZONTAL);
 		
-		CalendarComposite calendarComposite = new CalendarComposite(sashForm, SWT.BORDER, taskContainer);
-		TaskListComposite taskListComposite = new TaskListComposite(sashForm, SWT.BORDER, taskContainer);
+		CategoryListComposite taskListComposite2 = new CategoryListComposite(fullSashForm, SWT.BORDER, categoryContainer);
+		
+		SashForm rightSashForm = new SashForm(fullSashForm, SWT.SMOOTH | SWT.VERTICAL);
+
+		fullSashForm.setWeights(new int[]{2,7});
+		
+		CalendarComposite calendarComposite = new CalendarComposite(rightSashForm, SWT.BORDER, taskContainer);
+		TaskListComposite taskListComposite = new TaskListComposite(rightSashForm, SWT.BORDER, taskContainer);
 	}
 }
