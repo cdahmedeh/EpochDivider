c98712219f6818e276ddb97d2454db53d149ca91
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-27 09:31:41 -0400

    TimeBlock class cleanup.

--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/time/TimeBlock.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/time/TimeBlock.java
@@ -5,6 +5,9 @@ import org.joda.time.Days;
 import org.joda.time.Duration;
 
 public class TimeBlock {
+	
+	/* ---- Constructs ---- */
+
 	public TimeBlock(DateTime start, DateTime end) {
 		this.start = start;
 		this.end = end;
@@ -16,7 +19,10 @@ public class TimeBlock {
 		this.start = start;
 		this.end = start.plus(duration);
 	}
-		
+
+	
+	/* ---- Main Data ---- */
+	
 	private DateTime start = null;
 	public void setStart(DateTime start) {this.start = start;}
 	public DateTime getStart() {return start;}
@@ -24,10 +30,28 @@ public class TimeBlock {
 	private DateTime end = null;
 	public void setEnd(DateTime end) {this.end = end;}
 	public DateTime getEnd() {return end;}
+
 	
-	public Duration getDuration() {return new Duration(start, end);}
-	
-	public int daysSpaning() {return Days.daysBetween(start.toLocalDate(), end.toLocalDate()).getDays();}
-	
-	@Override public String toString() {return start.toString();}
+	/* ---- Reader Methods ---- */
+
+	/**
+	 * Get duration of TimeBlock.
+	 * 
+	 * @return Duration of TimeBlock.
+	 */
+	public Duration getDuration() {
+		return new Duration(start, end);
+	}
+
+	/**
+	 * Gives the number of days that are covered by the TimeBlock.
+	 * 
+	 * For example, a TimeBlock that starts on Jan 2 at 23:59 and ends on Jan 3
+	 * 00:01 spans 2 days even though it is less than 24 hours.
+	 * 
+	 * @return Number of days spanned by TimeBlock.
+	 */
+	public int daysSpaning() {
+		return Days.daysBetween(start.toLocalDate(), end.toLocalDate()).getDays();
+	}
 }
