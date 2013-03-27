9eb027a5e39a43ab605650caae6a62ccc78a643c
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-03-27 00:00:11 -0400

    View.java cleanup.

--- Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/calendar/View.java
+++ Proj_OrganizerApp_Vers3/src/org/cdahmedeh/orgapp/types/calendar/View.java
@@ -7,29 +7,48 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 
 public class View {
+	
+	/* ---- Constructs ---- */
+	
 	public View(LocalDate startDate, LocalDate endDate, LocalTime firstHour, LocalTime lastHour) {
 		this.startDate = startDate; this.endDate = endDate;
 		this.firstHour = firstHour;	this.lastHour = lastHour;
 	}
 	
 	
-	/* -- -- DATA -- -- */
+	/* ---- Main Data ---- */
 	
-	private LocalDate startDate = null;
-	public LocalDate getStartDate() {return startDate;}
-	public void setStartDate(LocalDate start) {this.startDate = start;}
+	private	LocalDate	startDate						= null;
+	public	LocalDate	getStartDate()					{return startDate;}
+	public	void		setStartDate(LocalDate start)	{this.startDate = start;}
 	
-	private LocalDate endDate = null;
-	public LocalDate getEndDate() {return endDate;}
-	public void setEndDate(LocalDate end) {this.endDate = end;}
+	private	LocalDate	endDate							= null;
+	public	LocalDate	getEndDate()					{return endDate;}
+	public	void		setEndDate(LocalDate end)		{this.endDate = end;}
 	
-	private LocalTime firstHour = null;
-	public LocalTime getFirstHour() {return firstHour;}
-	public void setFirstHour(LocalTime first) {this.firstHour = first;}
+	private	LocalTime	firstHour						= null;
+	public	LocalTime	getFirstHour()					{return firstHour;}
+	public	void		setFirstHour(LocalTime first)	{this.firstHour = first;}
 	
-	private LocalTime lastHour = null;
-	public LocalTime getLastHour() {return lastHour;}
-	public void setLastHour(LocalTime last) {this.lastHour = last;}
+	private	LocalTime	lastHour						= null;
+	public	LocalTime	getLastHour()					{return lastHour;}
+	public	void		setLastHour(LocalTime last)		{this.lastHour = last;}
+	
+	
+	/* ---- Compare Methods ---- */
+	
+	@Override
+	public int hashCode() {
+		return (startDate.toString() + endDate.toString()).hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return this.hashCode() == obj.hashCode();
+	}
+	
+	
+	/* ---- Reader Methods ---- */
 	
 	public int getNumberOfDaysVisible(){
 		return 1 + Days.daysBetween(startDate, endDate).getDays();
@@ -43,18 +62,12 @@ public class View {
 		return new Interval(startDate.toDateTimeAtStartOfDay(), endDate.toDateTimeAtStartOfDay().plusDays(1));
 	}
 	
+	
+	/* ---- Manipulate Methods ---- */
+	
 	public void moveAmountOfDays(int days){
 		this.setStartDate(this.getStartDate().plusDays(days));
 		this.setEndDate(this.getEndDate().plusDays(days));
 	}
-	
-	@Override
-	public int hashCode() {
-		return (startDate.toString() + endDate.toString()).hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return this.hashCode() == obj.hashCode();
-	}
+
 }
