fc320ed601d428f0eccf4a6d8c019513a9033be7
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-16 01:03:01 -0400

    Added buggy scrolling.

--- Proj_OrganizerApp_Vers3/ui-swing/org/cdahmedeh/orgapp/swing/calendar/BigCalendarPanel.java
+++ Proj_OrganizerApp_Vers3/ui-swing/org/cdahmedeh/orgapp/swing/calendar/BigCalendarPanel.java
@@ -3,10 +3,18 @@ package org.cdahmedeh.orgapp.swing.calendar;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 import javax.swing.JScrollBar;
 
 import org.cdahmedeh.orgapp.containers.BigContainer;
+import org.cdahmedeh.orgapp.types.calendar.View;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.ScrollBar;
+import org.joda.time.LocalTime;
+
 import net.miginfocom.swing.MigLayout;
 
 public class BigCalendarPanel extends JPanel {
@@ -26,14 +34,32 @@ public class BigCalendarPanel extends JPanel {
 		JPanel panel_1 = new DayLinePanel(bigContainer);
 		mainPanel.add(panel_1, "cell 1 0,grow");
 		
-		JPanel panel = new TimeLinePanel(bigContainer);
+		final JPanel panel = new TimeLinePanel(bigContainer);
 		mainPanel.add(panel, "cell 0 1,grow");
 		
-		JPanel calendarPanel = new CalendarPanel(bigContainer);
+		final JPanel calendarPanel = new CalendarPanel(bigContainer);
 		mainPanel.add(calendarPanel, "cell 1 1,alignx left,growy");
 		
-		JScrollBar scrollBar = new JScrollBar();
+		final JScrollBar scrollBar = new JScrollBar();
 		add(scrollBar, BorderLayout.EAST);
+		
+		
+		final View currentView = bigContainer.getCurrentView();
+		scrollBar.setValues(currentView.getFirstHour().getHourOfDay(), 1, 0, 24 - currentView.getNumberOfHoursVisible());
+		
+		scrollBar.addAdjustmentListener(new AdjustmentListener() {
+			
+			@Override
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+				int selection = scrollBar.getValue();
+				currentView.setLastHour(new LocalTime(selection+currentView.getNumberOfHoursVisible(),59,59,999));
+				currentView.setFirstHour(new LocalTime(selection, 0, 0));
+				calendarPanel.repaint();
+				panel.repaint();
+			}
+		});
+		
+		
 	}
 
 }
