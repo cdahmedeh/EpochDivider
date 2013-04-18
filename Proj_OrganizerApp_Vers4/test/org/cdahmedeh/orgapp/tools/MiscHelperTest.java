3fe72e79a4dd4f7dfe0439464f6ea1595ff859bd
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-18 01:16:05 -0400

    Added MiscHelperTest for Unit testing on MiscHelper.

---
+++ Proj_OrganizerApp_Vers4/test/org/cdahmedeh/orgapp/tools/MiscHelperTest.java
@@ -0,0 +1,29 @@
+package org.cdahmedeh.orgapp.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for MiscHelper
+ *  
+ * @author Ahmed El-Hajjar
+ */
+public class MiscHelperTest {
+	@Test public void safeTrimShouldRemoveOnlyWhiteSpaceBeforeAndAfterInputString(){
+		assertEquals("test", MiscHelper.safeTrim("test"));
+		assertEquals("te st", MiscHelper.safeTrim("te st"));
+		assertEquals("test", MiscHelper.safeTrim(" test "));
+		assertEquals("test", MiscHelper.safeTrim("test "));
+		assertEquals("test", MiscHelper.safeTrim(" test"));
+		assertEquals("test", MiscHelper.safeTrim(" test   "));
+		assertEquals("test", MiscHelper.safeTrim("   test   "));
+		assertEquals("test", MiscHelper.safeTrim("   test "));
+		assertEquals("test", MiscHelper.safeTrim(" 	test  	\r	\n   \t"));
+		assertEquals("multiple word test", 		MiscHelper.safeTrim(" multiple word test "));
+	}
+	
+	@Test public void safeTrimShouldGiveBlankStringIfInputIsNull(){
+		assertEquals("", MiscHelper.safeTrim(null));
+	}
+}
