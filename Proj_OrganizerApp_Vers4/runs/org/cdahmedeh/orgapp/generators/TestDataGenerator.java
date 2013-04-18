1766141ed8e7b26239fe771624f4992032078fa2
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-18 13:46:53 -0400

    Added TestDataGenerator to generate test data. Currently generates a list of contexts.

---
+++ Proj_OrganizerApp_Vers4/runs/org/cdahmedeh/orgapp/generators/TestDataGenerator.java
@@ -0,0 +1,23 @@
+package org.cdahmedeh.orgapp.generators;
+
+import java.util.ArrayList;
+
+import org.cdahmedeh.orgapp.types.context.Context;
+
+/**
+ * Contains method to generate some test data.
+ * 
+ * @author Ahmed El-Hajjar
+ */
+public class TestDataGenerator {
+	public ArrayList<Context> generateListOfContexts(){
+		ArrayList<Context> contextList = new ArrayList<>();
+		
+		contextList.add(new Context("Faith"));
+		contextList.add(new Context("Sleep"));
+		contextList.add(new Context("Relaxing"));
+		contextList.add(new Context("Exercise"));
+		
+		return contextList;
+	}
+}
