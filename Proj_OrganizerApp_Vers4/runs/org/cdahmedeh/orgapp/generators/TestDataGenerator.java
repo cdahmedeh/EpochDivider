dad259aa31e12bd8b445f305238236d0a5c3177d
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-18 16:51:12 -0400

    Add more data to Contexts generator. Contexts are now set to a ContextCategory as well.

--- Proj_OrganizerApp_Vers4/runs/org/cdahmedeh/orgapp/generators/TestDataGenerator.java
+++ Proj_OrganizerApp_Vers4/runs/org/cdahmedeh/orgapp/generators/TestDataGenerator.java
@@ -3,6 +3,7 @@ package org.cdahmedeh.orgapp.generators;
 import java.util.ArrayList;
 
 import org.cdahmedeh.orgapp.types.context.Context;
+import org.cdahmedeh.orgapp.types.context.ContextCategory;
 
 /**
  * Contains method to generate some test data.
@@ -13,10 +14,33 @@ public class TestDataGenerator {
 	public static ArrayList<Context> generateListOfContexts(){
 		ArrayList<Context> contextList = new ArrayList<>();
 		
-		contextList.add(new Context("Faith"));
-		contextList.add(new Context("Sleep"));
-		contextList.add(new Context("Relaxing"));
-		contextList.add(new Context("Exercise"));
+		ContextCategory essentialsContextCategory = new ContextCategory("Essentials");
+		
+		Context faithContext = new Context("Faith");
+		faithContext.setCategory(essentialsContextCategory);
+		contextList.add(faithContext);
+		
+		Context sleepContext = new Context("Sleep");
+		sleepContext.setCategory(essentialsContextCategory);
+		contextList.add(sleepContext);
+		
+		Context relaxingContext = new Context("Relaxing");
+		relaxingContext.setCategory(essentialsContextCategory);
+		contextList.add(relaxingContext);
+		
+		Context exerciseContext = new Context("Exercise");
+		exerciseContext.setCategory(essentialsContextCategory);
+		contextList.add(exerciseContext);
+		
+		ContextCategory universityContextCategory = new ContextCategory("University");
+		
+		Context studyContext = new Context("Study");
+		studyContext.setCategory(universityContextCategory);
+		contextList.add(studyContext);
+		
+		Context coursesContext = new Context("Courses");
+		coursesContext.setCategory(universityContextCategory);
+		contextList.add(coursesContext);
 		
 		return contextList;
 	}
