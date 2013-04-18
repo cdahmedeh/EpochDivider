7e1403fe2462e38e6890d5c8df4955f7bb9cd6d6
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-18 14:00:54 -0400

    ContextListPanel can now be populated with ArrayList<> of contexts.

--- Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/context/ContextListPanel.java
+++ Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/context/ContextListPanel.java
@@ -7,6 +7,16 @@ import java.awt.Dimension;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 
+import org.cdahmedeh.orgapp.generators.TestDataGenerator;
+import org.cdahmedeh.orgapp.types.context.Context;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.gui.TableFormat;
+import ca.odell.glazedlists.swing.AdvancedTableModel;
+import ca.odell.glazedlists.swing.DefaultEventListModel;
+import ca.odell.glazedlists.swing.GlazedListsSwing;
+
 public class ContextListPanel extends JPanel {
 	private static final long serialVersionUID = -8250528552031443184L;
 	
@@ -21,6 +31,11 @@ public class ContextListPanel extends JPanel {
 		setPreferredSize(new Dimension(ContextListPanelDefaults.DEFAULT_CONTEXT_PANEL_WIDTH, -1));
 		setLayout(new BorderLayout());
 		
+		createContextListTable();
+		prepareContextListTableModel();
+	}
+
+	private void createContextListTable() {
 		contextListPane = new JScrollPane();
 		add(contextListPane, BorderLayout.CENTER);
 		
@@ -29,4 +44,17 @@ public class ContextListPanel extends JPanel {
 		contextListPane.setViewportView(contextListTable);
 	}
 
+	/**
+	 * Set the Table Model for the Context List Table.
+	 */
+	private void prepareContextListTableModel() {
+		TableFormat<Context> contextTableFormat = new ContextListTableFormat();
+		EventList<Context> contextEventList = new BasicEventList<>();
+		contextEventList.addAll(TestDataGenerator.generateListOfContexts());
+		
+		AdvancedTableModel<Context> contextListTableModel = 
+				GlazedListsSwing.eventTableModelWithThreadProxyList(contextEventList, contextTableFormat);
+		
+		contextListTable.setModel(contextListTableModel);
+	}
 }
--- Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/context/ContextListPanelDefaults.java
+++ Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/context/ContextListPanelDefaults.java
@@ -2,4 +2,6 @@ package org.cdahmedeh.orgapp.swingui.context;
 
 public class ContextListPanelDefaults {
 	public static final int DEFAULT_CONTEXT_PANEL_WIDTH = 200;
+	
+	public static final int COLUMN_NAME = 0;
 }
