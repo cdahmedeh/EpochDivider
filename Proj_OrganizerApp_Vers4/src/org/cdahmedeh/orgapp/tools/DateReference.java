f4c3470fa0e8a4244222c3d1b5e32d2d6c90e4e1
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-25 19:50:32 -0400

    Fix DataReference so that it does not call itself forever.

--- Proj_OrganizerApp_Vers4/src/org/cdahmedeh/orgapp/tools/DateReference.java
+++ Proj_OrganizerApp_Vers4/src/org/cdahmedeh/orgapp/tools/DateReference.java
@@ -3,12 +3,13 @@ package org.cdahmedeh.orgapp.tools;
 import org.joda.time.DateTime;
 
 /**
- * Contains a reference to DateReference.getNow(). Can be changed for testing purposes.
+ * Contains a reference to DateTime.now(). Can be changed for testing 
+ * purposes.
  * 
  * @author Ahmed El-Hajjar
  */
 public class DateReference {
 	public static DateTime getNow() {
-		return DateReference.getNow();
+		return DateTime.now();
 	}
 }
