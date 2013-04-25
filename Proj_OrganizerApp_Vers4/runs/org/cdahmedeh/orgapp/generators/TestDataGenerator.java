abcfa353ac5748a1a02f8785519dc6b808344625
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-25 13:38:53 -0400

    Some tasks should not have due dates in test data.

--- Proj_OrganizerApp_Vers4/runs/org/cdahmedeh/orgapp/generators/TestDataGenerator.java
+++ Proj_OrganizerApp_Vers4/runs/org/cdahmedeh/orgapp/generators/TestDataGenerator.java
@@ -148,7 +148,9 @@ public class TestDataGenerator {
 			Task task = new Task("Task " + i);
 			task.setEstimate(Duration.standardMinutes(new Random().nextInt(20*60)));
 			task.setContext(contextList.get(new Random().nextInt(contextList.size())));
-			task.setDue(DateTime.now().plusMinutes(new Random().nextInt(1000)*30));
+			if (new Random().nextInt(5) == 0) {
+				task.setDue(DateTime.now().plusMinutes(new Random().nextInt(1000)*30));
+			}
 			task.setCompleted(new Random().nextBoolean());
 			taskList.add(task);
 		}
