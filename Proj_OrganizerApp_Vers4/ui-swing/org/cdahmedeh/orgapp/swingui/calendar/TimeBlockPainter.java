648df74123a1de9e7cd5c80d487df4fe5f5f01c9
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-28 15:29:47 -0400

    Added horribly slow text cropping technique.

--- Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/calendar/TimeBlockPainter.java
+++ Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/calendar/TimeBlockPainter.java
@@ -3,11 +3,13 @@ package org.cdahmedeh.orgapp.swingui.calendar;
 import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 import org.cdahmedeh.orgapp.tools.DateReference;
+import org.cdahmedeh.orgapp.tools.MiscHelper;
 import org.cdahmedeh.orgapp.types.calendar.View;
 import org.cdahmedeh.orgapp.types.task.Task;
 import org.cdahmedeh.orgapp.types.time.TimeBlock;
@@ -101,9 +103,9 @@ public class TimeBlockPainter {
 //				g.setColor(new Color(0.5f, 0.5f, 0.5f, 1f));
 //	/**/	}
 			
-			g.drawString(task.getTitle().substring(0, Math.min(12, task.getTitle().length())), rect.x+5, rect.y+15);
-			if (rect.height > 30) g.drawString(tBeginTime.toString("HH:mm") + "-" + tEndTime.toString("HH:mm"), rect.x+5, rect.y+30);
-//			drawString(g, task.getTitle() + " " + tBeginTime.toString("HH:mm") + "-" + tEndTime.toString("HH:mm"), rect.x+5, rect.y+15, rect.width - 5, rect.height - 20);
+//			g.drawString(task.getTitle().substring(0, Math.min(12, task.getTitle().length())), rect.x+5, rect.y+15);
+//			if (rect.height > 30) g.drawString(tBeginTime.toString("HH:mm") + "-" + tEndTime.toString("HH:mm"), rect.x+5, rect.y+30);
+			drawString(g, task.getTitle() + " " + tBeginTime.toString("HH:mm") + "-" + tEndTime.toString("HH:mm"), rect.x+5, rect.y+15, rect.width - 5, rect.height - 20);
 //			if (rect.height < 30) g.drawString(tBeginTime.toString("HH:mm") + "-" + tEndTime.toString("HH:mm"), rect.x+5, rect.y+30);
 		}
 		
@@ -117,39 +119,55 @@ public class TimeBlockPainter {
 	}
 	
 	//From: http://stackoverflow.com/questions/400566/full-justification-with-a-java-graphics-drawstring-replacement
-	//TODO: Improve
+	//TODO: Improve, slow, not loop proof.
 	//TODO: What if a word is just too long?
 	public static void drawString(Graphics g, String s, int x, int y, int width, int height)
 	{
-		// FontMetrics gives us information about the width,
-		// height, etc. of the current Graphics object's Font.
-		FontMetrics fm = g.getFontMetrics();
+		String[] split = s.split(" ");
+		ArrayDeque<String> text = MiscHelper.toArrayDeque(split);
+		FontMetrics fontMetrics = g.getFontMetrics();
 
-		int lineHeight = fm.getHeight();
-
-		int curX = x;
-		int curY = y;
-
-		String[] words = s.split(" ");
-
-		for (String word : words)
-		{
-			if (curY > y+height) break;
+		int yPrintPos = y;
+		int spaceWidth = fontMetrics.stringWidth(" ");
+		
+		while (!text.isEmpty()){
+			int xPrintPos = x;
+			int lineLength = 0;
 			
-			// Find out thw width of the word.
-			int wordWidth = fm.stringWidth(word + " ");
-
-			// If text exceeds the width, then move to next line.
-			if (curX + wordWidth >= x + width)
-			{
-				curY += lineHeight;
-				curX = x;
+			while (!text.isEmpty()) {
+				String nextWord = text.pop();
+				
+				int nextWordWidth = fontMetrics.stringWidth(nextWord);
+				
+				System.out.println(nextWordWidth);
+				System.out.println(width);
+				
+				if (nextWordWidth + spaceWidth >= width && lineLength == 0) {
+					System.out.println("Word too long: " + nextWord);
+					g.drawString(drawCroppedString(fontMetrics, nextWord, width), xPrintPos+lineLength, yPrintPos);
+					break;
+				} else if (lineLength + nextWordWidth + spaceWidth < width) {
+					g.drawString(nextWord, xPrintPos+lineLength, yPrintPos);
+				} else {
+					text.push(nextWord);
+					System.out.println("Word next line: " + nextWord);
+					break;
+				}
+				
+				lineLength += nextWordWidth + spaceWidth;
 			}
+			
+			yPrintPos += fontMetrics.getHeight();
+			
+			if (yPrintPos-y > height) break;
+		}
+	}
 
-			g.drawString(word, curX, curY);
-
-			// Move over to the right for next word.
-			curX += wordWidth;
+	private static String drawCroppedString(FontMetrics metrics, String nextWord, int width) {
+		if (nextWord.isEmpty() || metrics.stringWidth(nextWord) < width){
+			return nextWord;
+		} else {
+			return drawCroppedString(metrics, nextWord.substring(0, nextWord.length()-1), width);
 		}
 	}
 }
