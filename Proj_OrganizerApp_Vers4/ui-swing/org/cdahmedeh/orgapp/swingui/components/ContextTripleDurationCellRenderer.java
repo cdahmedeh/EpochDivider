a0094b0d1415f3c3f580ccc3499a696b7ac55b54
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-04-29 20:14:58 -0400

    Minor tweak for painting.

--- Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/components/ContextTripleDurationCellRenderer.java
+++ Proj_OrganizerApp_Vers4/ui-swing/org/cdahmedeh/orgapp/swingui/components/ContextTripleDurationCellRenderer.java
@@ -3,23 +3,29 @@ package org.cdahmedeh.orgapp.swingui.components;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import javax.swing.BorderFactory;
+
 public class ContextTripleDurationCellRenderer extends TripleDurationCellRenderer {
 	private static final long serialVersionUID = -8255299386801283428L;
 
 	@Override
 	public void paint(Graphics g) {
 //		super.paint(g);
-//		super.paintComponent(g);
-		super.paint(g);
+		super.paintComponent(g);
+//		super.paint(g);
+		super.setBorder(BorderFactory.createEmptyBorder());
 		
 		double totalProgress = value.getEstimate().getStandardSeconds();
 		double secondProgress = value.getTotalScheduled().getStandardSeconds();
 		double firstProgress = value.getTotalPassed().getStandardSeconds();
 		
+		Color light = new Color(0, 0, 0, 5);
 		Color brighter = new Color(0, 0, 0, 20);
 		
+		g.setColor(light);
+		g.fillRoundRect(2, 2, this.getWidth()-4, this.getHeight()-4, 3, 3);
 		g.setColor(brighter);
-		g.fillRect(0, 0, (int)(this.getWidth()*(secondProgress/totalProgress)), this.getHeight());
-		g.fillRect(0, 0, (int)(this.getWidth()*(firstProgress/totalProgress)), this.getHeight());
+		g.fillRoundRect(2, 2, (int)((this.getWidth()-4)*(secondProgress/totalProgress)), this.getHeight()-4, 3, 3);
+		g.fillRoundRect(2, 2, (int)((this.getWidth()-4)*(firstProgress/totalProgress)), this.getHeight()-4, 3 ,3);
 	}
 }
