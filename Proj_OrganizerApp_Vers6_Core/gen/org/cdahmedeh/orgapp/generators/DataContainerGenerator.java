b68cc8fe261aa3b2359bba7db06fe3eb35e80453
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-07-04 18:57:31 -0400

    Added DataContainerGenerator to generate test instances of a data container.

---
+++ Proj_OrganizerApp_Vers6_Core/gen/org/cdahmedeh/orgapp/generators/DataContainerGenerator.java
@@ -0,0 +1,30 @@
+package org.cdahmedeh.orgapp.generators;
+
+import org.cdahmedeh.orgapp.types.containers.DataContainer;
+import org.cdahmedeh.orgapp.types.context.Category;
+import org.cdahmedeh.orgapp.types.context.Context;
+
+/**
+ * Used to generate DataContainer objects for test purposes.
+ * 
+ * @author Ahmed El-Hajjar
+ */
+public class DataContainerGenerator {
+	public static DataContainer generateBasicDataContainer(){
+		DataContainer dataContainer = new DataContainer();
+		
+		Category essentialsCategory = new Category("Essentials");
+		essentialsCategory.addContext(new Context("Faith"));
+		essentialsCategory.addContext(new Context("Sleep"));
+		essentialsCategory.addContext(new Context("News and Breakfast"));
+		
+		Category leisureCategory = new Category("Leisure");
+		leisureCategory.addContext(new Context("Gaming"));
+		leisureCategory.addContext(new Context("Tinkering"));
+		
+		dataContainer.getCategories().add(essentialsCategory);
+		dataContainer.getCategories().add(leisureCategory);
+		
+		return dataContainer;
+	}
+}
