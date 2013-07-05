5e064bf483311afb23ad7a858919d2f2562e4190
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-07-04 23:19:38 -0400

    Remove Vers6 as no rewrite is needed.

--- Proj_OrganizerApp_Vers6_SwingUI/imt/org/cdahmedeh/orgapp/imt/icons/Icons.java
+++
@@ -1,6 +0,0 @@
-package org.cdahmedeh.orgapp.imt.icons;
-
-public class Icons {
-	public static final String TASKS = "/org/cdahmedeh/orgapp/imt/icons/tasks.gif";
-	public static final String ADD = "/org/cdahmedeh/orgapp/imt/icons/add.gif";
-}
--- Proj_OrganizerApp_Vers6_SwingUI/src/org/cdahmedeh/orgapp/swingui/contextlist/ContextListConstants.java
+++
@@ -1,9 +0,0 @@
-package org.cdahmedeh.orgapp.swingui.contextlist;
-
-public class ContextListConstants {
-	public static final int CONTEXT_LIST_DEFAULT_WIDTH = 240;
-	public static final int CONTEXT_LIST_DEFAULT_HEIGHT = 0;
-	
-	public static final int COLUMN_NAME_INDEX = 0;
-	public static final String COLUMN_NAME_LABEL = "Name";
-}
