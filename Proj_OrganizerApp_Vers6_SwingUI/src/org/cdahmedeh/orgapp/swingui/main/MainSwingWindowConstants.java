6c754e4d09465863cc1941e824c505a0d7e8a157
Ahmed El-Hajjar <cdahmedeh@gmail.com>
2013-07-04 20:52:33 -0400

    Make main window slightly wider (800 -> 960 px).

--- Proj_OrganizerApp_Vers6_SwingUI/src/org/cdahmedeh/orgapp/swingui/main/MainSwingWindowConstants.java
+++ Proj_OrganizerApp_Vers6_SwingUI/src/org/cdahmedeh/orgapp/swingui/main/MainSwingWindowConstants.java
@@ -3,6 +3,6 @@ package org.cdahmedeh.orgapp.swingui.main;
 public class MainSwingWindowConstants {
 	public static final int MAIN_WINDOW_DEFAULT_X_POS = 100;
 	public static final int MAIN_WINDOW_DEFAULT_Y_POS = 100;
-	public static final int MAIN_WINDOW_DEFAULT_WIDTH = 800;
+	public static final int MAIN_WINDOW_DEFAULT_WIDTH = 960;
 	public static final int MAIN_WINDOW_DEFAULT_HEIGHT = 600;
 }
